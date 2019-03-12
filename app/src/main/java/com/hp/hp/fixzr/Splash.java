ed7fdc30b7bc45c69d558b958d87bcd9fc66128c
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2019-03-12 11:00:47 +0530

    Initial commit

---
+++ app/src/main/java/com/hp/hp/fixzr/Splash.java
@@ -0,0 +1,13 @@
+package com.hp.hp.fixzr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
